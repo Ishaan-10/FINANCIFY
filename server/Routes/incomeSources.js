ffc94d90ee70dd6f2d47b0467b31da510da3a09d
Ishaan-10 <b.ishaan10@gmail.com>
2021-08-01 09:19:19 +0530

    added all routes

---
+++ server/Routes/incomeSources.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/',async(req,res)=>{
+
+})
+
+// update
+router.post('/',async(req,res)=>{
+    
+})
+
+// delete
+router.delete('/',async(req,res)=>{
+
+})
+
+// modify
+router.put('/',async(req,res)=>{
+
+})
+
+
+module.exports = router;
---
+++ server/Routes/transactions.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+// create
+// Need to update the total amount spend as well
+router.get('/',async(req,res)=>{
+
+})
+
+// update
+router.post('/',async(req,res)=>{
+    
+})
+
+// delete
+router.delete('/',async(req,res)=>{
+
+})
+
+// modify
+router.put('/',async(req,res)=>{
+
+})
+
+module.exports = router;
