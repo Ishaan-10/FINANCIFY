b0b2d8b54be6909935911312b8fdc53c0cc3bad9
darshpat01 <darshpat01@gmail.com>
2021-08-02 02:23:46 +0530

    no changes made

--- server/Routes/authRoutes.js
+++ server/Routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/login',(req,res)=>{
+router.get('/login', (req, res) => {
     console.log("login route")
+    res.render()
 })
-router.get('/register',(req,res)=>{
+router.get('/register', (req, res) => {
     console.log("register route")
 })
 
