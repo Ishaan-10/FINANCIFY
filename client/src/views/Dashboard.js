5dc20921e9929b4fcd6f9bb4ddbeb0632c047d62
unknown <meadamann2002@gmail.com>
2021-08-03 23:42:09 +0530

    Dashboard restructure

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ChartistGraph from "react-chartist";
+import TransactionRow from "components/TransactionRow";
+
 // react-bootstrap components
 import {
   Badge,
@@ -33,7 +35,7 @@ function Dashboard() {
                   <Col xs="7">
                     <div className="numbers">
                       <p className="card-category">Total Amount Spent </p>
-                      <Card.Title as="h4">150GB</Card.Title>
+                      <Card.Title as="h4">₹ 150</Card.Title>
                     </div>
                   </Col>
                 </Row>
@@ -59,7 +61,7 @@ function Dashboard() {
                   <Col xs="7">
                     <div className="numbers">
                       <p className="card-category">Monthly Income</p>
-                      <Card.Title as="h4">$ 1,345</Card.Title>
+                      <Card.Title as="h4">₹ 1,345</Card.Title>
                     </div>
                   </Col>
                 </Row>
@@ -110,7 +112,7 @@ function Dashboard() {
                   </Col>
                   <Col xs="7">
                     <div className="numbers">
-                      <p className="card-category">Followers</p>
+                      <p className="card-category">Recurring Payments</p>
                       <Card.Title as="h4">+45K</Card.Title>
                     </div>
                   </Col>
@@ -130,68 +132,27 @@ function Dashboard() {
           <Col md="8">
             <Card>
               <Card.Header>
-                <Card.Title as="h4">Users Behavior</Card.Title>
-                <p className="card-category">24 Hours performance</p>
+                <Card.Title as="h4">Recent Transactions</Card.Title>
+                <p className="card-category">Last 24 Hours</p>
               </Card.Header>
-              <Card.Body>
-                <div className="ct-chart" id="chartHours">
-                  <ChartistGraph
-                    data={{
-                      labels: [
-                        "9:00AM",
-                        "12:00AM",
-                        "3:00PM",
-                        "6:00PM",
-                        "9:00PM",
-                        "12:00PM",
-                        "3:00AM",
-                        "6:00AM",
-                      ],
-                      series: [
-                        [287, 385, 490, 492, 554, 586, 698, 695],
-                        [67, 152, 143, 240, 287, 335, 435, 437],
-                        [23, 113, 67, 108, 190, 239, 307, 308],
-                      ],
-                    }}
-                    type="Line"
-                    options={{
-                      low: 0,
-                      high: 800,
-                      showArea: false,
-                      height: "245px",
-                      axisX: {
-                        showGrid: false,
-                      },
-                      lineSmooth: true,
-                      showLine: true,
-                      showPoint: true,
-                      fullWidth: true,
-                      chartPadding: {
-                        right: 50,
-                      },
-                    }}
-                    responsiveOptions={[
-                      [
-                        "screen and (max-width: 640px)",
-                        {
-                          axisX: {
-                            labelInterpolationFnc: function (value) {
-                              return value[0];
-                            },
-                          },
-                        },
-                      ],
-                    ]}
-                  />
-                </div>
+              <Card.Body className="table-full-width table-responsive px-0">
+                <Table className="table-hover table-striped">
+                  <thead>
+                    <tr>
+                      <th className="border-0">Serial No.</th>
+                      <th className="border-0">Name</th>
+                      <th className="border-0">Amount</th>
+                      <th className="border-0">Category</th>
+                      <th className="border-0">Mode of Payment</th>
+                      <th className="border-0">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <TransactionRow/>
+                  </tbody>
+                </Table>
               </Card.Body>
               <Card.Footer>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Open <i className="fas fa-circle text-danger"></i>
-                  Click <i className="fas fa-circle text-warning"></i>
-                  Click Second Time
-                </div>
                 <hr></hr>
                 <div className="stats">
                   <i className="fas fa-history"></i>
@@ -203,7 +164,7 @@ function Dashboard() {
           <Col md="4">
             <Card>
               <Card.Header>
-                <Card.Title as="h4">Email Statistics</Card.Title>
+                <Card.Title as="h4">Categories</Card.Title>
                 <p className="card-category">Last Campaign Performance</p>
               </Card.Header>
               <Card.Body>
@@ -238,92 +199,28 @@ function Dashboard() {
           <Col md="6">
             <Card>
               <Card.Header>
-                <Card.Title as="h4">2017 Sales</Card.Title>
+                <Card.Title as="h4">Time Until Next Cycle</Card.Title>
                 <p className="card-category">All products including Taxes</p>
               </Card.Header>
               <Card.Body>
-                <div className="ct-chart" id="chartActivity">
-                  <ChartistGraph
-                    data={{
-                      labels: [
-                        "Jan",
-                        "Feb",
-                        "Mar",
-                        "Apr",
-                        "Mai",
-                        "Jun",
-                        "Jul",
-                        "Aug",
-                        "Sep",
-                        "Oct",
-                        "Nov",
-                        "Dec",
-                      ],
-                      series: [
-                        [
-                          542,
-                          443,
-                          320,
-                          780,
-                          553,
-                          453,
-                          326,
-                          434,
-                          568,
-                          610,
-                          756,
-                          895,
-                        ],
-                        [
-                          412,
-                          243,
-                          280,
-                          580,
-                          453,
-                          353,
-                          300,
-                          364,
-                          368,
-                          410,
-                          636,
-                          695,
-                        ],
-                      ],
-                    }}
-                    type="Bar"
-                    options={{
-                      seriesBarDistance: 10,
-                      axisX: {
-                        showGrid: false,
-                      },
-                      height: "245px",
-                    }}
-                    responsiveOptions={[
-                      [
-                        "screen and (max-width: 640px)",
-                        {
-                          seriesBarDistance: 5,
-                          axisX: {
-                            labelInterpolationFnc: function (value) {
-                              return value[0];
-                            },
-                          },
-                        },
-                      ],
-                    ]}
-                  />
-                </div>
+                <Row>
+                  <Col xs="5">
+                    <div className="icon-big text-center icon-clock">
+                      <i className="nc-icon nc-chart text-clock"></i>
+                    </div>
+                  </Col>
+                  <Col xs="7">
+                    <div className="numbers">
+                      <Card.Title as="h4">24:23:32:00</Card.Title>
+                    </div>
+                  </Col>
+                </Row>
               </Card.Body>
               <Card.Footer>
-                <div className="legend">
-                  <i className="fas fa-circle text-info"></i>
-                  Tesla Model S <i className="fas fa-circle text-danger"></i>
-                  BMW 5 Series
-                </div>
                 <hr></hr>
                 <div className="stats">
-                  <i className="fas fa-check"></i>
-                  Data information certified
+                  <i className="fas fa-redo mr-1"></i>
+                  Update Now
                 </div>
               </Card.Footer>
             </Card>
