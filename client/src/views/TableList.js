be1456ebfc0709dc02f32e084f75068ed102026e
unknown <meadamann2002@gmail.com>
2021-08-03 16:02:04 +0530

    Added Transaction

--- client/src/views/TableList.js
+++ client/src/views/TableList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import TransactionRow from "components/TransactionRow";
 
 // react-bootstrap components
 import {
@@ -40,54 +41,7 @@ function TableList() {
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Dakota Rice</td>
-                      <td>$36,738</td>
-                      <td>Niger</td>
-                      <td>Oud-Turnhout</td>
-                      <td>Delete/Edit</td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Minerva Hooper</td>
-                      <td>$23,789</td>
-                      <td>Curaçao</td>
-                      <td>Sinaai-Waas</td>
-                      <td>Delete/Edit</td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>Sage Rodriguez</td>
-                      <td>$56,142</td>
-                      <td>Netherlands</td>
-                      <td>Baileux</td>
-                      <td>Delete/Edit</td>
-                    </tr>
-                    <tr>
-                      <td>4</td>
-                      <td>Philip Chaney</td>
-                      <td>$38,735</td>
-                      <td>Korea, South</td>
-                      <td>Overland Park</td>
-                      <td>Delete/Edit</td>
-                    </tr>
-                    <tr>
-                      <td>5</td>
-                      <td>Doris Greene</td>
-                      <td>$63,542</td>
-                      <td>Malawi</td>
-                      <td>Feldkirchen in Kärnten</td>
-                      <td>Delete/Edit</td>
-                    </tr>
-                    <tr>
-                      <td>6</td>
-                      <td>Mason Porter</td>
-                      <td>$78,615</td>
-                      <td>Chile</td>
-                      <td>Gloucester</td>
-                      <td>Delete/Edit</td>
-                    </tr>
+                    <TransactionRow/>
                   </tbody>
                 </Table>
               </Card.Body>
