bafca732987b3ffdf7dbced2dab75ba24caf0070
Ishaan-10 <b.ishaan10@gmail.com>
2021-08-08 22:42:03 +0530

    updated goal card

--- client/src/views/Maps.js
+++ client/src/views/Maps.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 // react-bootstrap components
-import { Badge,
+import {
+  Badge,
   Button,
   Card,
   Form,
   ProgressBar,
   Container,
   Row,
-  Col, } from "react-bootstrap";
+  Col,
+} from "react-bootstrap";
 
 function Maps() {
   const percentage = 73
@@ -16,89 +18,111 @@ function Maps() {
     <>
       <Container fluid>
         <Row>
-        <Col md="12">
+          <Col md="12">
             <Card>
               <Card.Header>
                 <Card.Title as="h4">Goal Progress</Card.Title>
               </Card.Header>
-              
+
               <Card.Body>
-              <div className="progressBar">
-                <ProgressBar now={percentage} label={`${percentage}% completed`} />
-              </div>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div>
+                    Currently Saved: 45000
+                  </div>
+                  <div>
+                    Goal Amount: 100000
+                  </div>
+                </div>
+                <div className="progressBar">
+                  <ProgressBar now={percentage} label={`${percentage}% completed`} />
+                </div>
+                {/* <div className="my-2 col-5">
+                  <Form.Control
+                    placeholder="Add Amount"
+                    type="Number"
+                  ></Form.Control>
+                  <Button>Add Amount</Button>
+                </div> */}
+                  <Form className="form-inline my-3 float-right">
+                    <Form.Control
+                      placeholder="Add Amount"
+                      type="Number"
+                    ></Form.Control>
+                    <Button className="ml-2 btn btn-fill">Add</Button>
+                  </Form>
               </Card.Body>
             </Card>
           </Col>
           <Col md="12">
             <Card>
-            <Card.Header>
+              <Card.Header>
                 <Card.Title as="h4">Add your Goals</Card.Title>
                 <p className="card-category">
                   This will help you to manage your finance goals.
                 </p>
-            </Card.Header>
-            <Form>
-            <Row>
-                <Col className="pl-4" md="6">
-                  <Form.Group>
-                    <label>Goal Description</label>
+              </Card.Header>
+              <Form>
+                <Row>
+                  <Col className="pl-4" md="6">
+                    <Form.Group>
+                      <label>Goal Description</label>
                       <Form.Control
                         defaultValue="Save For a Home"
                         placeholder="Goal"
                         type="text"
                       ></Form.Control>
                     </Form.Group>
-                </Col>
-                <Col className="pl-1" md="6">
-                  <Form.Group>
-                    <label>Goal Amount</label>
-                    <Form.Control
+                  </Col>
+                  <Col className="pl-1" md="6">
+                    <Form.Group>
+                      <label>Goal Amount</label>
+                      <Form.Control
                         defaultValue="1000000"
                         placeholder="Amount"
                         type="number"
-                    ></Form.Control>
+                      ></Form.Control>
                     </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col className="pl-4" md="6">
-                  <Form.Group>
-                    <label>Date Started</label>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="pl-4" md="6">
+                    <Form.Group>
+                      <label>Date Started</label>
                       <Form.Control
                         placeholder="Start Time"
                         type="date"
                       ></Form.Control>
                     </Form.Group>
-                </Col>
-                <Col className="pl-1" md="6">
-                  <Form.Group>
-                    <label>Deadline</label>
-                    <Form.Control
+                  </Col>
+                  <Col className="pl-1" md="6">
+                    <Form.Group>
+                      <label>Deadline</label>
+                      <Form.Control
                         placeholder="Date"
                         type="date"
-                    ></Form.Control>
+                      ></Form.Control>
                     </Form.Group>
-                </Col>
-              </Row>
-              <Row>
-                <Col className="pl-4" md="12">
-                  <Form.Group>
-                    <label>How will you complete it?</label>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col className="pl-4" md="12">
+                    <Form.Group>
+                      <label>How will you complete it?</label>
                       <Form.Control
                         defaultValue="Saving in Fixed Deposists"
                         placeholder="Completion"
                         type="text"
                       ></Form.Control>
                     </Form.Group>
-                </Col>
-              </Row>
-              <div className="d-grid gap-2 pl-2">
+                  </Col>
+                </Row>
+                <div className="d-grid gap-2 pl-2">
                   <Button className="btn-fill pull-right" variant="primary" type="submit">
                     Add Goal
                   </Button>
-              </div>
-              <br />
-            </Form>
+                </div>
+                <br />
+              </Form>
             </Card>
           </Col>
         </Row>
