143f021be13c59d489c00c63a93805b7bef19c97
unknown <meadamann2002@gmail.com>
2021-08-11 16:09:20 +0530

    News feed changed

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import img1 from '../images/gadgets.jpeg'
+
 import {
     Badge,
     Button,
@@ -22,11 +24,28 @@ export default function News() {
                 <Card.Text>
                   Since you are buying a lot of electronics, we suggest to keep an eye on sales/discounts.
                 </Card.Text>
-                <Button variant="primary">Save Money</Button>
+                <Button className="btn-fill pull-right" variant="secondary">Save Money</Button>
               </Card.Body>
               <hr></hr>
               <Card.Footer className="text-muted">Till 2 days</Card.Footer>
         </Card>
+        <div class="card">
+          <div class="card-horizontal">
+            <div class="img-square-wrapper">
+              <img class="" src={img1} alt="Card image cap"></img>
+            </div>
+            <div class="card-body">
+              <h4 class="card-title">Card title</h4>
+              <hr></hr>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <Button className="btn-fill pull-right" variant="secondary">Save Money</Button>
+            </div>
+
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
         </>
     )
 }
